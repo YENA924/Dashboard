6b346d0fe6d7085114a283777ab9ffccb16e635b
yena <wyena924@gmail.com>
2022-04-05 15:11:28 +0900

    - ui 작업을 위한 vuetify 3 설치 - EVUI 라이브러리 설치

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router/router'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 import EVUI from 'evui'
 
+loadFonts()
+
 createApp(App)
-.use(router)
-.use(EVUI)
-.mount('#app')
+  .use(vuetify)
+  .use(EVUI)
+  .mount('#app')
