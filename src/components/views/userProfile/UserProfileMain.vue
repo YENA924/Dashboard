3b679da128ffdec5b1567dd413d3652eadf40198
yena <wyena924@gmail.com>
2022-04-12 09:42:29 +0900

    - 스타일 삭제

--- src/components/views/userProfile/UserProfileMain.vue
+++ src/components/views/userProfile/UserProfileMain.vue
@@ -16,28 +16,3 @@ export default {
   components: { EditUserInfo, UserInfo }
 }
 </script>
-
-<style>
-  .ev-button.type-text {
-    color: #ffffff;
-    background-color: #FF7BA9;
-  }
-  
-  .ev-text-field .ev-input,
-  .ev-text-field .ev-textarea {
-    border: 1px solid #FFBED8;
-  }
-
-  .ev-text-field:hover .ev-input,
-  .ev-text-field:focus .ev-input,
-  .ev-text-field:hover .ev-textarea,
-  .ev-text-field:focus .ev-textarea {
-    border: 1px solid #FFBED8;
-  }
-  .ev-text-field .ev-input:hover,
-  .ev-text-field .ev-input:focus,
-  .ev-text-field .ev-textarea:hover,
-  .ev-text-field .ev-textarea:focus {
-    border: 1px solid #FFBED8;
-  }
-</style>
